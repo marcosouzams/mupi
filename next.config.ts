4c2e25cd0d278c20b035a4d022cf8fc6a6fcc5f9
Marco Souza <marcoantonio99d@gmail.com>
2025-09-03 14:22:44 -0300

    netlify

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
-const { i18n } = require('./next-i18next.config');
-
 const nextConfig: NextConfig = {
-  i18n,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
