beac2baafb220053910882c67b8447d8566de553
Marco Souza <marcoantonio99d@gmail.com>
2025-11-06 15:21:13 -0300

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ClientLayout } from "@/components";
 
@@ -33,6 +34,24 @@ export default function RootLayout({
         <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
       <body className={`${inter.variable} antialiased font-inter`}>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-NTZVTPKD7N"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NTZVTPKD7N');
+            `,
+          }}
+        />
+        
         <ClientLayout>
           {children}
         </ClientLayout>
@@ -40,3 +59,4 @@ export default function RootLayout({
     </html>
   );
 }
+
